8644b214086f32e76c091ac7ca792fe10811cad2
Jacob Lee Seaton <jacobseaton@Jacobs-MacBook-Pro.local>
2022-11-11 11:50:59 -0600

    added Java

--- 99-lines-of-code/app.js
+++ 99-lines-of-code/app.js
@@ -1,13 +1,33 @@
 let friends = ['John', 'Carlos', 'Morgan', 'Arnold', 'Stephanie']
 
-for (let i = 0; i < friends.length; i++) {
-    for (j = 99; j > 0; j--) {
-        if (j > 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
-        } else if (j === 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+/* i loop nested inside f(friends) loop */
+const button = document.getElementsByClassName('btn')[0];
+
+button.addEventListener('click', function(){
+    
+for (var f = 0; f < friends.length; f++) {
+    let h2 = document.createElement('h2');
+    let h2Text = document.createTextNode(`${friends[f]}`);
+    h2.appendChild(h2Text);
+    document.body.appendChild(h2);
+    // div.appendChild('h2');
+
+
+    for (var i = 99; i >= 1; i--) {
+        if (i == 1) {
+            let paragraph = document.createElement('p');
+            document.body.appendChild(paragraph);
+            let pText = document.createTextNode(`${(i)} line of code in the file, ${(i)} line of code; ${friends[f]} strikes one out, clears it all out, no more lines of code in the file`);
+            paragraph.appendChild(pText);
+            //console.log(`${(i)} line of code in the file, ${(i)} line of code; ${friends[f]} strikes one out, clears it all out, no more lines of code in the file`);
         } else {
-            console.log(j + "line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+            let paragraph2 = document.createElement('p');
+            document.body.appendChild(paragraph2);
+            let pText2 = document.createTextNode(`${(i)} lines of code in the file, ${(i)} lines of code; ${friends[f]} strikes one out, clears it all out, ${(i - 1)} lines of code in the file`);
+            paragraph2.appendChild(pText2);
+      
+            // console.log(`${(i)} lines of code in the file, ${(i)} lines of code; ${friends[f]} strikes one out, clears it all out, ${(i - 1)} lines of code in the file`);   
         }
-    }
-}
+    } 
+};
+});
